1428213266909754bd4d7aae3382c49799c0a659
marine <marine@ws20>
2012-11-22 16:28:14 +0300

    File input dialog added.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,17 @@
 package main;
 
+import java.io.File;
+
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.FileDialog;
 
 public class Main {
 	private static Text textInputPath;
@@ -21,9 +27,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		File fileInput;
+		File fileOutput;
+		
 		Display display = Display.getDefault();
-		Shell shell = new Shell();
+		final Shell shell = new Shell();
+		shell.setImage(SWTResourceManager.getImage("logo.png"));
 		shell.setSize(435, 125);
+		shell.setMinimumSize(435, 125);
 		shell.setText("Reed chart");
 		
 		Label LabelFileInput = new Label(shell, SWT.NONE);
@@ -41,6 +52,18 @@ public class Main {
 		textOutputPath.setBounds(80, 37, 260, 23);
 		
 		Button ButtonBrowseInput = new Button(shell, SWT.NONE);
+		ButtonBrowseInput.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog fdOpen = new FileDialog(shell, SWT.OPEN);
+				fdOpen.setText("Open");
+				String[] filterExt = {"*.str", "*.*"};
+				fdOpen.setFilterExtensions(filterExt);
+				String selected = fdOpen.open();
+				textInputPath.setText(selected);
+				
+				
+			}
+		});
 		ButtonBrowseInput.setBounds(346, 10, 68, 23);
 		ButtonBrowseInput.setText("Browse");
 		
@@ -49,6 +72,11 @@ public class Main {
 		ButtonBrowseOuput.setText("Browse");
 		
 		Button ButtonCreateDXF = new Button(shell, SWT.NONE);
+		ButtonCreateDXF.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				WriteDXF.WriteToDXF();
+			}
+		});
 		ButtonCreateDXF.setBounds(155, 66, 110, 23);
 		ButtonCreateDXF.setText("Create DXF");
 
@@ -59,8 +87,6 @@ public class Main {
 				display.sleep();
 			}
 		}
-		
-		WriteDXF.WriteToDXF();
 		
 	}
 }
