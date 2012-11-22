3ddeded58622f164ddc649305a8575e7243c8a94
marine <marine@ws20>
2012-11-22 15:05:45 +0300

    Work with main form.

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,8 +8,8 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
 
 public class Main {
-	private static Text text;
-	private static Text text_1;
+	private static Text textInputPath;
+	private static Text textOutputPath;
 
 	public Main() {
 		// TODO Auto-generated constructor stub
@@ -23,34 +23,34 @@ public class Main {
 		
 		Display display = Display.getDefault();
 		Shell shell = new Shell();
-		shell.setSize(450, 120);
+		shell.setSize(435, 125);
 		shell.setText("Reed chart");
 		
-		Label lblNewLabel = new Label(shell, SWT.NONE);
-		lblNewLabel.setBounds(10, 10, 49, 13);
-		lblNewLabel.setText("File input");
+		Label LabelFileInput = new Label(shell, SWT.NONE);
+		LabelFileInput.setBounds(10, 10, 64, 21);
+		LabelFileInput.setText("File input");
 		
-		Label lblNewLabel_1 = new Label(shell, SWT.NONE);
-		lblNewLabel_1.setBounds(10, 37, 49, 13);
-		lblNewLabel_1.setText("File output");
+		Label labelFileOutput = new Label(shell, SWT.NONE);
+		labelFileOutput.setBounds(10, 37, 64, 23);
+		labelFileOutput.setText("File output");
 		
-		text = new Text(shell, SWT.BORDER);
-		text.setBounds(80, 10, 260, 19);
+		textInputPath = new Text(shell, SWT.BORDER);
+		textInputPath.setBounds(80, 10, 260, 21);
 		
-		text_1 = new Text(shell, SWT.BORDER);
-		text_1.setBounds(80, 37, 260, 19);
+		textOutputPath = new Text(shell, SWT.BORDER);
+		textOutputPath.setBounds(80, 37, 260, 23);
 		
-		Button btnNewButton = new Button(shell, SWT.NONE);
-		btnNewButton.setBounds(346, 10, 68, 23);
-		btnNewButton.setText("Browse");
+		Button ButtonBrowseInput = new Button(shell, SWT.NONE);
+		ButtonBrowseInput.setBounds(346, 10, 68, 23);
+		ButtonBrowseInput.setText("Browse");
 		
-		Button btnNewButton_1 = new Button(shell, SWT.NONE);
-		btnNewButton_1.setBounds(346, 37, 68, 23);
-		btnNewButton_1.setText("Browse");
+		Button ButtonBrowseOuput = new Button(shell, SWT.NONE);
+		ButtonBrowseOuput.setBounds(346, 37, 68, 23);
+		ButtonBrowseOuput.setText("Browse");
 		
-		Button btnNewButton_2 = new Button(shell, SWT.NONE);
-		btnNewButton_2.setBounds(183, 62, 68, 23);
-		btnNewButton_2.setText("Create DXF");
+		Button ButtonCreateDXF = new Button(shell, SWT.NONE);
+		ButtonCreateDXF.setBounds(155, 66, 110, 23);
+		ButtonCreateDXF.setText("Create DXF");
 
 		shell.open();
 		shell.layout();
